1aff95ae22886f520b385d4de79763d28da412ce
Eric Chen <eric.chen.us@gmail.com>
2023-11-28 12:36:16 -0500

    notifications

--- raincheck-io/src/Form.js
+++ raincheck-io/src/Form.js
@@ -8,6 +8,8 @@ const Form = () => {
   const [endTime, setEndTime] = useState('05:00 PM');
   const [timezone, setTimezone] = useState('EST');
   const [wantNotifications, setWantNotifications] = useState(false);
+  const [useHeaders, setUseHeaders] = useState(true);
+  const [notificationEmail, setNotificationEmail] = useState(''); // New state for email
 
   // Function to handle changes in the event name input
   const handleEventNameChange = (e) => {
@@ -34,6 +36,9 @@ const Form = () => {
     setWantNotifications(e.target.checked);
   };
 
+  const handleNotificationEmailChange = (e) => {
+    setNotificationEmail(e.target.value);
+  };
   // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -66,12 +71,16 @@ const Form = () => {
       <h2 className="text-4xl font-bold mb-3 p-4">Create a New Event</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label
-            htmlFor="eventName"
-            className="block text-sm font-medium text-blue-500"
-          >
-            Event Name <span className="text-red-500">*</span>
-          </label>
+          {/* if useHeader is true, then include Event Name header */}
+          {useHeaders && (
+            <label
+              htmlFor="eventName"
+              className="block text-sm font-medium text-blue-500"
+            >
+              Event Name <span className="text-red-500">*</span>
+            </label>
+          )}
+
           <input
             type="text"
             id="eventName"
@@ -84,9 +93,12 @@ const Form = () => {
           />
         </div>
 
-        <label className="block text-sm font-medium text-blue-500 mb-2">
-          Calendar Type <span className="text-red-500">*</span>
-        </label>
+        {useHeaders && (
+          <label className="block text-sm font-medium text-blue-500 mb-2">
+            Calendar Type <span className="text-red-500">*</span>
+          </label>
+        )}
+
         <div className="mb-4 flex justify-center">
           <div className="flex">
             <button
@@ -109,10 +121,12 @@ const Form = () => {
             </button>
           </div>
         </div>
+        {useHeaders && (
+          <label className="block text-sm font-medium text-blue-500">
+            Time Range <span className="text-red-500">*</span>
+          </label>
+        )}
 
-        <label className="block text-sm font-medium text-blue-500">
-          Time Range <span className="text-red-500">*</span>
-        </label>
         <div className="mb-4 flex justify-center">
           <select
             id="startTime"
@@ -170,9 +184,12 @@ const Form = () => {
           </select>
         </div>
 
-        <label className="block text-sm font-medium text-blue-500 ml-2 mt-3">
-          Timezone <span className="text-red-500">*</span>
-        </label>
+        {useHeaders && (
+          <label className="block text-sm font-medium text-blue-500 ml-2 mt-3">
+            Timezone <span className="text-red-500">*</span>
+          </label>
+        )}
+
         <div className="mb-4 flex justify-center">
           <select
             id="timezone"
@@ -190,22 +207,48 @@ const Form = () => {
           </select>
         </div>
 
-        <div className="ml-4 flex items-center justify-center">
-          <label
-            htmlFor="wantNotifications"
-            className="ml-2 text-sm text-blue-500"
-          >
-            Want notifications?
-          </label>
+        <div className="ml-4">
+          <div className="flex items-center justify-center">
+            <label
+              htmlFor="wantNotifications"
+              className="text-sm text-blue-500"
+            >
+              Want notifications?
+            </label>
 
-          <input
-            type="checkbox"
-            id="wantNotifications"
-            name="wantNotifications"
-            checked={wantNotifications}
-            onChange={handleWantNotificationsChange}
-            className="form-checkbox h-4 w-4 text-blue-500 ml-4"
-          />
+            <input
+              type="checkbox"
+              id="wantNotifications"
+              name="wantNotifications"
+              checked={wantNotifications}
+              onChange={handleWantNotificationsChange}
+              className="form-checkbox h-4 w-4 text-blue-500 ml-4"
+            />
+          </div>
+
+          {wantNotifications && (
+            <div className="mb-4 mt-2">
+              {useHeaders && (
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-blue-500"
+                >
+                  Email Address <span className="text-red-500">*</span>
+                </label>
+              )}
+
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={notificationEmail}
+                onChange={handleNotificationEmailChange}
+                placeholder="Enter your email"
+                className="mt-1 p-2 border rounded-md ml-2 placeholder-gray-500 w-full"
+                required
+              />
+            </div>
+          )}
         </div>
 
         <button

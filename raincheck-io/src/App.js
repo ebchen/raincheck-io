df066d4151ff1d3e9e26aeeed8337f9a3ffcd27e
eradewan <dewanera@gmail.com>
2023-12-05 01:00:11 -0500

    availability and copy link component

--- raincheck-io/src/App.js
+++ raincheck-io/src/App.js
@@ -7,6 +7,8 @@ import React from 'react';
 import Cal from './Calendar';
 import DOW from './DOW';
 import { useState } from 'react';
+import AvailabilityComponent from './AvailabilityComponent';
+import CopyLinkComponent from './CopyLinkComponent';
 
 function App() {
   const [mode, setMode] = useState("specificDates");
@@ -17,6 +19,7 @@ function App() {
       </header>
 
       <Sidebar /> */}
+      
       <div className="flex justify-center items-center h-screen bg-slate-100">
         <div className="flex flex-row justify-around items-center w-full max-w-6xl mx-auto p-4">
           <Form setMode={setMode} />
---
+++ raincheck-io/src/AvailabilityComponent.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+
+const AvailabilityComponent = () => {
+  const [name, setName] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSyncCalendar = () => {
+    // Handle syncing with Google Calendar 
+  };
+
+  const handleAddAvailability = () => {
+    // Handle adding availability 
+  };
+
+  return (
+    <div className="w-full lg:w-1/3 bg-white rounded-md shadow-md items-center justify-center py-8 px-8 mx-28">
+      <h2 className="text-2xl font-bold mb-0 p-3 text-center">Add your Availability</h2>
+      
+      <div className="flex flex-col items-center">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="Enter your name"
+          className="mt-1 p-2 border rounded-md placeholder-gray-500 w-full mb-4"
+          required
+        />   
+
+        <button
+          type="button"
+          onClick={handleSyncCalendar}
+          className="bg-white font-bold text-blue-500 border border-blue-500 py-2 rounded-md text-base hover:bg-blue-100 w-full mb-4  flex items-center justify-center"
+        >
+          <FcGoogle className="w-6 h-6 mr-1" />
+          <span className="ml-2">Sync your Google Calendar</span>
+        </button>
+
+        <button
+          type="button"
+          onClick={handleAddAvailability}
+          className="bg-blue-500 text-white px-20 py-2 rounded-md text-xl hover:bg-blue-700 mb-2"
+        >
+          Add Availability
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AvailabilityComponent;
